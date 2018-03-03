303176b28f9ed94f9e239839c64d8f10dcc00322
James Hagewood <hagewoodjc@gmail.com>
2018-03-02 23:27:07 -0600

    In progress

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach(Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
+            }
+
+            //return View(results);
+
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            Product[] productArray =
+            {
+                new Product { Name = "Kayak", Price = 275M },
+                new Product { Name = "Lifejacket", Price = 48.95M },
+                new Product { Name = "Soccer basll", Price = 19.50M },
+                new Product { Name = "Corner flag", Price = 34.95M },
+            };
+
+            decimal arrayTotal = productArray.FilterByPrice(20).TotalPrices();
+
+            return View("Index", new string[]
+            {
+                $"Array Total: {arrayTotal:C2}"
+            });            
+        }
+    }
+}
